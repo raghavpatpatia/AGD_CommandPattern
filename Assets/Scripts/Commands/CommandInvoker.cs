0dd455b0deb54a6af2a90abef5f4341e43cd63d4
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-03-09 04:22:41 +0530

    Command Pattern
    Added a Command Invoker.

---
+++ Assets/Scripts/Commands/CommandInvoker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+public class CommandInvoker
+{
+    private Stack<ICommand> commandRegistry = new Stack<ICommand>();
+    public void ProcessCommand(ICommand commandToProcess)
+    {
+        ExecuteCommand(commandToProcess);
+        RegisterCommand(commandToProcess);
+    }
+    public void ExecuteCommand(ICommand commandToExecute) => commandToExecute.Execute();
+    public void RegisterCommand(ICommand commandToRegister) => commandRegistry.Push(commandToRegister);
+}

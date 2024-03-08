df24b2d7d38058f22e2876d2477e8634c97c8c0a
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-03-09 00:42:54 +0530

    Command Pattern
    Created an abstract class UnitCommand which inherits ICommand interface to store all information related to unit's command.

---
+++ Assets/Scripts/Commands/Abstract Commands/UnitCommand.cs
@@ -0,0 +1,15 @@
+﻿using Command.Player;
+
+public abstract class UnitCommand : ICommand
+{
+    public int ActorUnitID;
+    public int TargetUnitID;
+    public int ActorPlayerID;
+    public int TargetPlayerID;
+
+    protected UnitController ActorUnit;
+    protected UnitController TargetUnit;
+
+    public abstract void Execute();
+    public abstract bool WillHitTarget();
+}
